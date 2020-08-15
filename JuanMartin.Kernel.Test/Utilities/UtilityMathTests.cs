83fecce2bc07b166b139b34e237f73dd7468834e
jmbotero <jbotero@hotmail.com>
2020-08-15 18:47:15 -0400

    Add algorithm to calulate square root by sustraction

---
+++ JuanMartin.Kernel.Test/Utilities/UtilityMathTests.cs
@@ -0,0 +1,20 @@
+﻿using JuanMartin.Kernel.Utilities;
+using NUnit.Framework;
+
+namespace JuanMartin.Kernel.Utilities.Tests
+{
+    [TestFixture]
+    public class UtilityMathTests
+    {
+        [Test]
+        public void SqrtBySubstraction_SqrtResultMultipliedByItselfShouldBeEqualToOriginalNumber()
+        {
+            for (double expected_number = 0.001; expected_number < 100; expected_number += 0.5d)
+            {
+                double actual_number = UtilityMath.Sqrt_By_Substraction(expected_number);
+
+                Assert.AreEqual(expected_number, actual_number * actual_number);
+            }
+        }
+    }
+}
