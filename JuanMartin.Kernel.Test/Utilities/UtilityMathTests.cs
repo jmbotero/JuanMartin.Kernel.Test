ac605f707aa360e344c06fcb715492f7e50c7505
jmbotero <jbotero@hotmail.com>
2020-09-06 18:18:22 -0400

    f]ix unit test execution

--- JuanMartin.Kernel.Test/Utilities/UtilityMathTests.cs
+++ JuanMartin.Kernel.Test/Utilities/UtilityMathTests.cs
@@ -1,11 +1,14 @@
 ﻿using JuanMartin.Kernel.Utilities;
 using NUnit.Framework;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace JuanMartin.Kernel.Utilities.Tests
 {
     [TestFixture]
     public class UtilityMathTests
     {
+        #region Sqrt_By_Substraction tests
         [Test]
         public void SqrtBySubstraction_SqrtResultMultipliedByItselfShouldBeEqualToOriginalNumber()
         {
@@ -15,6 +18,30 @@ namespace JuanMartin.Kernel.Utilities.Tests
 
                 Assert.AreEqual(expected_number, actual_number * actual_number);
             }
+        } 
+        #endregion
+
+        #region GetNumericMasks tests
+        [Test]
+        public void NumberShouldHaveOnePatternForAtLeastTwoDigitsRepeatinng()
+        {
+            var masks = UtilityMath.GetNumericMasks(123426);
+            Assert.IsTrue(masks.Where(m => Regex.Matches(m, @"\\1").Count == 1).Count() == 1);
+
+            var masks2 = UtilityMath.GetNumericMasks(223426);
+            Assert.IsFalse(masks2.Where(m => Regex.Matches(m, @"\\1").Count == 1).Count() == 1);
+
         }
+
+        [Test]
+        public void NumberShouldReturnOneValidRegexPatternForEverySetofRepeatedDigits()
+        {
+            var masks = UtilityMath.GetNumericMasks(121313);
+
+            Assert.AreEqual(2, masks.Count);
+            Assert.IsTrue(masks.Contains(@"\d\d\d(\d)\d\1"));
+            Assert.IsTrue(masks.Contains(@"(\d)\d\1\d\1\d"));
+        }
+        #endregion
     }
 }
